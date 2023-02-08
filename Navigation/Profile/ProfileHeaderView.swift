4c0544c3f51bccb42f5ba115b4a233a5ed0a11d5
Ruslan <usenrn@gmail.com>
2023-02-09 00:06:50 +0400

    2. Add UI elements

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -9,4 +9,55 @@ import UIKit
 
 class ProfileHeaderView: UIView {
     
+    let imageView: UIImageView = {
+        let image = UIImageView()
+        image.backgroundColor = .systemPink
+        image.image = UIImage(named: "Cat")
+        image.layer.borderWidth = 3
+        image.layer.borderColor = UIColor.white.cgColor
+        image.layer.cornerRadius = 75
+        image.contentMode = .scaleToFill
+        image.clipsToBounds = true
+        return image
+    }()
+    
+    let button: UIButton = {
+        $0.backgroundColor = .link
+        $0.setTitle("Show status", for: .normal)
+        $0.tintColor = .white
+        $0.layer.cornerRadius = 4
+        $0.layer.shadowOffset = CGSize(width: 4, height: 4)
+        $0.layer.shadowRadius = 4
+        $0.layer.shadowColor = UIColor.black.cgColor
+        $0.layer.shadowOpacity = 0.7
+        return $0
+    }(UIButton())
+    
+    let nameLabel: UILabel = {
+        $0.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        $0.textColor = .black
+        $0.text = "Hipster Cat"
+        return $0
+    }(UILabel())
+    
+    let textLabel: UILabel = {
+        $0.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        $0.textColor = .gray
+        $0.text = "Waiting for something"
+
+        return $0
+    }(UILabel())
+    
+    
+    func addViews() {
+        addSubview(imageView)
+        addSubview(button)
+        addSubview(nameLabel)
+        addSubview(textLabel)
+    }
+   
+    
+    
+    
+    
 }
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -8,10 +8,25 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    
+    private let screenWidth = UIScreen.main.bounds.width
+    private let profileHeaderView = ProfileHeaderView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .lightGray
+        profileHeaderView.addViews()
+        
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        view.addSubview(profileHeaderView)
+        profileHeaderView.frame = view.frame
+        profileHeaderView.imageView.frame = CGRect(x: 16, y: view.safeAreaInsets.top + 16, width: 150, height: 150)
+        profileHeaderView.button.frame = CGRect(x: 16, y: view.safeAreaInsets.top + profileHeaderView.imageView.bounds.height + 32, width: screenWidth - 32, height: 50)
+        profileHeaderView.nameLabel.frame = CGRect(x: Int(profileHeaderView.imageView.bounds.width) + 32, y: Int(view.safeAreaInsets.top) + 27, width: Int(screenWidth - profileHeaderView.imageView.bounds.width) - 48, height: 50)
+        profileHeaderView.textLabel.frame = CGRect(x: Int(profileHeaderView.imageView.bounds.width) + 32, y: Int(view.safeAreaInsets.top + profileHeaderView.imageView.bounds.height) - 52, width: Int(screenWidth - profileHeaderView.imageView.bounds.width) - 48, height: 50)
     }
 
 }
