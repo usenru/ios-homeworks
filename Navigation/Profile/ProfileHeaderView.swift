c869e10d419470bec468472c091ff43014b11437
Ruslan <usenrn@gmail.com>
2023-02-08 18:51:26 +0400

    1. Add ProfileHeaderView, ProfileViewController

---
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation
+//
+//  Created by Admin on 08.02.23.
+//
+
+import UIKit
+
+class ProfileHeaderView: UIView {
+    
+}
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -11,11 +11,7 @@ class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
-
+        view.backgroundColor = .lightGray
     }
-    
-
 
 }
