3ef5b153e86392718306a554f8ef985a2976626f
Ruslan <usenrn@gmail.com>
2023-02-15 19:44:08 +0400

    2. Add layout

--- Navigation/MainTabBarController.swift
+++ Navigation/MainTabBarController.swift
@@ -11,6 +11,7 @@ class MainTabBarController: UITabBarController {
     
     private let feedVC = FeedViewController()
     private let profileVC = ProfileViewController()
+    private let logInVC = LogInViewController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,18 +23,19 @@ class MainTabBarController: UITabBarController {
         
         let firstNavigationController = UINavigationController(rootViewController: feedVC)
         let secondNavigationController = UINavigationController(rootViewController: profileVC)
+        let loginNavController = UINavigationController(rootViewController: logInVC)
         
         tabBar.backgroundColor = .white
         tabBar.alpha = 0.5
         
         feedVC.tabBarItem.title = "Лента"
-        feedVC.tabBarItem.image = UIImage(systemName: "newspaper.fill")
+        feedVC.tabBarItem.image = UIImage(systemName: "house.fill")
         
-        profileVC.tabBarItem.title = "Профиль"
-        profileVC.title = "Профиль"
-        profileVC.tabBarItem.image = UIImage(systemName: "person.text.rectangle.fill")
+        logInVC.tabBarItem.title = "Профиль"
+        logInVC.title = "Профиль"
+        logInVC.tabBarItem.image = UIImage(systemName: "person.fill")
         
-        viewControllers = [firstNavigationController, secondNavigationController]
+        viewControllers = [firstNavigationController, loginNavController]
         
     }
 
--- Navigation/Profile/LogInViewController.swift
+++ Navigation/Profile/LogInViewController.swift
@@ -9,4 +9,136 @@ import UIKit
 
 class LogInViewController: UIViewController {
     
+    
+    
+    private let scrollView: UIScrollView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UIScrollView())
+    
+    private let contentView: UIView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UIView())
+    
+    private let logoImageView: UIImageView = {
+        $0.image = UIImage(named: "logo")
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UIImageView())
+    
+    private lazy var userNameTextField: UITextField = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.layer.borderColor = UIColor.lightGray.cgColor
+        $0.layer.borderWidth = 0.5
+        $0.layer.cornerRadius = 10
+        $0.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
+        $0.textColor = .black
+        $0.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        $0.tintColor = UIColor(named: "accentColor")
+        $0.autocapitalizationType = .none
+        $0.backgroundColor = .systemGray6
+        $0.placeholder = "  Email or phone"
+        $0.delegate = self
+    
+        return $0
+    }(UITextField())
+    
+    private lazy var passwordTextField: UITextField = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.layer.borderColor = UIColor.lightGray.cgColor
+        $0.layer.borderWidth = 0.5
+        $0.layer.cornerRadius = 10
+        $0.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        $0.textColor = .black
+        $0.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        $0.tintColor = UIColor(named: "accentColor")
+        $0.autocapitalizationType = .none
+        $0.isSecureTextEntry = true
+        $0.backgroundColor = .systemGray6
+        $0.placeholder = "  Password"
+        $0.delegate = self
+        return $0
+    }(UITextField())
+    
+    private lazy var logInButton: UIButton = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.layer.cornerRadius = 10
+        $0.setBackgroundImage(UIImage(named: "blue_pixel"), for: .normal)
+        $0.clipsToBounds = true
+        $0.setTitle("Log In", for: .normal)
+        $0.tintColor = .white
+        $0.alpha = $0.isSelected ? 0.8 : 1
+        $0.addTarget(self, action: #selector(tappedAction), for: .touchUpInside)
+        
+      
+        return $0
+    }(UIButton())
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.isHidden = true
+        view.backgroundColor = .white
+        layout()
+    }
+    
+    @objc private func tappedAction() {
+        let profileVC = ProfileViewController()
+        navigationController?.pushViewController(profileVC, animated: true)
+    }
+    
+    private func layout() {
+        
+        let subViews = [scrollView, contentView, logoImageView, userNameTextField, passwordTextField, logInButton]
+        subViews.forEach {
+            view.addSubview($0)
+        }
+        
+        NSLayoutConstraint.activate([
+        
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+
+            logoImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 120),
+            logoImageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            logoImageView.heightAnchor.constraint(equalToConstant: 100),
+            logoImageView.widthAnchor.constraint(equalToConstant: 100),
+            
+            userNameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 120),
+            userNameTextField.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            userNameTextField.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            userNameTextField.heightAnchor.constraint(equalToConstant: 50),
+            
+            passwordTextField.topAnchor.constraint(equalTo: userNameTextField.bottomAnchor),
+            passwordTextField.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            passwordTextField.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 50),
+            
+            logInButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 16),
+            logInButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            logInButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+//            logInButton.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16),
+            logInButton.heightAnchor.constraint(equalToConstant: 50),
+            
+        ])
+        
+    }
+    
+}
+
+extension LogInViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
 }
