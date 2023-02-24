5e188f86ca7d2fc7a0baf1277b4a4779e072225c
Ruslan Usen <usenrn@gmail.com>
2023-02-25 00:29:57 +0400

    2. Add PhotosTableViewCell layouts

--- Navigation/PhotosTableViewCell.swift
+++ Navigation/PhotosTableViewCell.swift
@@ -13,88 +13,105 @@ class PhotosTableViewCell: UITableViewCell {
     
     // MARK: - Private properties
     let photosLabel: UILabel = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.text = "Photos"
         $0.font = UIFont.systemFont(ofSize: 24, weight: .bold)
         $0.textColor = .black
         
         return $0
     }(UILabel())
     
-    let arrowLabel: UILabel = {
-        $0.font.withSize(12)
+    let arrowImageView: UIImageView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.image = UIImage(systemName: "arrow.right")
+        $0.tintColor = .black
         return $0
-    }(UILabel())
+    }(UIImageView())
     
     let photo1ImageView: UIImageView = {
-        $0.image = UIImage(named: "iamges-1")
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.image = UIImage(named: "images-1")
         $0.layer.cornerRadius = 6
         $0.clipsToBounds = true
         return $0
     }(UIImageView())
     
     let photo2ImageView: UIImageView = {
-        $0.image = UIImage(named: "iamges-2")
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.image = UIImage(named: "images-2")
         $0.layer.cornerRadius = 6
         $0.clipsToBounds = true
         return $0
     }(UIImageView())
     
     let photo3ImageView: UIImageView = {
-        $0.image = UIImage(named: "iamges-3")
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.image = UIImage(named: "images-3")
         $0.layer.cornerRadius = 6
         $0.clipsToBounds = true
         return $0
     }(UIImageView())
     
     let photo4ImageView: UIImageView = {
-        $0.image = UIImage(named: "iamges-4")
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.image = UIImage(named: "images-4")
         $0.layer.cornerRadius = 6
         $0.clipsToBounds = true
         return $0
     }(UIImageView())
+    
+    let stackView: UIStackView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.axis = .horizontal
+        $0.spacing = 8
+        return $0
+    }(UIStackView())
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        initialization()
+        layout()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
-}
-
-private extension PhotosTableViewCell {
-    func initialization() {
+    
+    private func layout() {
+        addSubview(photosLabel)
+        addSubview(arrowImageView)
+        addSubview(stackView)
+        [photo1ImageView, photo2ImageView, photo3ImageView, photo4ImageView].forEach {
+            stackView.addArrangedSubview($0)
+        }
+        
         NSLayoutConstraint.activate([
         
             photosLabel.topAnchor.constraint(equalTo: topAnchor, constant: 12),
             photosLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12),
             
-            arrowLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12),
-            arrowLabel.centerYAnchor.constraint(equalTo: photosLabel.centerYAnchor),
+            arrowImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12),
+            arrowImageView.centerYAnchor.constraint(equalTo: photosLabel.centerYAnchor),
             
-            photo1ImageView.topAnchor.constraint(equalTo: photosLabel.bottomAnchor, constant: 12),
-            photo1ImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12),
-            photo1ImageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -12),
+            stackView.topAnchor.constraint(equalTo: photosLabel.bottomAnchor, constant: 12),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -12),
+          
             photo1ImageView.widthAnchor.constraint(equalToConstant: imagesSize),
             photo1ImageView.heightAnchor.constraint(equalToConstant: imagesSize),
-            
-            photo2ImageView.leadingAnchor.constraint(equalTo: photo1ImageView.trailingAnchor, constant: 8),
-            photo2ImageView.centerYAnchor.constraint(equalTo: photo1ImageView.centerYAnchor),
-            photo2ImageView.heightAnchor.constraint(equalTo: photo1ImageView.heightAnchor),
+       
+            photo2ImageView.widthAnchor.constraint(equalTo: photo1ImageView.widthAnchor),
             photo2ImageView.heightAnchor.constraint(equalTo: photo1ImageView.heightAnchor),
             
-            photo3ImageView.leadingAnchor.constraint(equalTo: photo2ImageView.trailingAnchor, constant: 8),
-            photo3ImageView.centerYAnchor.constraint(equalTo: photo1ImageView.centerYAnchor),
-            photo3ImageView.heightAnchor.constraint(equalTo: photo1ImageView.heightAnchor),
+ 
+            photo3ImageView.widthAnchor.constraint(equalTo: photo1ImageView.widthAnchor),
             photo3ImageView.heightAnchor.constraint(equalTo: photo1ImageView.heightAnchor),
             
-            photo4ImageView.leadingAnchor.constraint(equalTo: photo3ImageView.trailingAnchor, constant: 8),
-            photo4ImageView.centerYAnchor.constraint(equalTo: photo1ImageView.centerYAnchor),
-            photo4ImageView.heightAnchor.constraint(equalTo: photo1ImageView.heightAnchor),
-            photo4ImageView.heightAnchor.constraint(equalTo: photo1ImageView.heightAnchor),
-            photo4ImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12)
+            photo4ImageView.widthAnchor.constraint(equalTo: photo1ImageView.widthAnchor),
+            photo4ImageView.heightAnchor.constraint(equalTo: photo1ImageView.heightAnchor)
             
         ])
     }
+
 }
+
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -15,6 +15,7 @@ class ProfileViewController: UIViewController {
         tableView.delegate = self
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.register(PostTableViewCell.self, forCellReuseIdentifier: PostTableViewCell.identifier)
+        tableView.register(PhotosTableViewCell.self, forCellReuseIdentifier: String(describing: PhotosTableViewCell.self))
         return tableView
     }()
 
@@ -31,34 +32,56 @@ class ProfileViewController: UIViewController {
         view.addSubview(tableView)
         self.tableView.rowHeight = UITableView.automaticDimension
         
-        
         NSLayoutConstraint.activate([
-        
             tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-          
         ])
     }
-
 }
 
 extension ProfileViewController: UITableViewDataSource, UITableViewDelegate {
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        2
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        postArray.count
+        if section == 0 {
+            return 1
+        } else {
+            return postArray.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: PostTableViewCell.identifier, for: indexPath) as! PostTableViewCell
-        cell.setupCell(post: postArray[indexPath.row])
-        return cell
+        
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: PhotosTableViewCell.self), for: indexPath) as! PhotosTableViewCell
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: PostTableViewCell.identifier, for: indexPath) as! PostTableViewCell
+            cell.setupCell(post: postArray[indexPath.row])
+            return cell
+        }
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        return headerTableView
+        if section == 0 {
+            return headerTableView
+
+        } else {
+            return nil
+        }
     }
     
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if section == 1 {
+            return 0
+        } else {
+            return UITableView.automaticDimension
+        }
+    }
 }
 
